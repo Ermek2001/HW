c0a5668d819579d80ddb66257aa3422229449dae
ермек <ermek@MacBook-Pro-ermek.local>
2020-07-14 16:38:33 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        public static void String ( int age, int temperature){
+            System.out.println(age);
+            System.out.println(temperature); {
+
+            }
+
+            return "Возраст человека: " + temperature
+
+
+
+
+
+        }
+
+        // write your code here
+    }
+}
